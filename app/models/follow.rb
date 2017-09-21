7dbf76b1f3f0331194a15a8326b1cf3406948562
Raghu Betina <raghu@firstdraft.com>
2017-09-21 15:50:20 +0000

    Direct association between users and follows

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,6 +1,10 @@
 class Follow < ApplicationRecord
   # Direct associations
 
+  belongs_to :follower,
+             :class_name => "User",
+             :counter_cache => true
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :follows,
+             :foreign_key => "follower_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
