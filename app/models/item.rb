d2f3296149be82b0ddc4cfdc5d9985d18a8c23f4
Raghu Betina <raghu@firstdraft.com>
2017-09-21 15:50:25 +0000

    Direct association between vendors and items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   # Direct associations
 
+  belongs_to :vendor
+
   belongs_to :user
 
   # Indirect associations
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,6 +1,9 @@
 class Vendor < ApplicationRecord
   # Direct associations
 
+  has_many   :items,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
