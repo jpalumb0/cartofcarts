83f44491ba4d323cd086ea4a0f2048dbfe2bc6a9
Raghu Betina <raghu@firstdraft.com>
2017-09-21 15:51:07 +0000

    Display only signed in users follows on index

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -10,7 +10,7 @@ class FollowsController < ApplicationController
   end
 
   def index
-    @follows = Follow.all
+    @follows = current_user.follows.page(params[:page]).per(10)
 
     render("follows/index.html.erb")
   end
